b58aa7b9d23e4d50d855a805f661d64e12ccb89a
ocustodio <cstdpvictor@gmail.com>
2024-04-05 17:39:33 -0300

    created attendee DTO, service and details

--- src/main/java/rocketseat/com/passin/repositories/CheckInRepostiroy.java
+++
@@ -1,7 +0,0 @@
-package rocketseat.com.passin.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rocketseat.com.passin.domain.checkin.CheckIn;
-
-public interface CheckInRepostiroy extends JpaRepository<CheckIn, Integer> {
-}
