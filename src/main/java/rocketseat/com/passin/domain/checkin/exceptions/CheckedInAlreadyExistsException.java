030c3b3e1b07033093af8a89292e87784e67e99d
ocustodio <cstdpvictor@gmail.com>
2024-04-08 15:31:13 -0300

    created register check in method and exception

---
+++ src/main/java/rocketseat/com/passin/domain/checkin/exceptions/CheckedInAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package rocketseat.com.passin.domain.checkin.exceptions;
+
+public class CheckedInAlreadyExistsException extends RuntimeException{
+    public CheckedInAlreadyExistsException(String message) {
+        super(message);
+    }
+}
